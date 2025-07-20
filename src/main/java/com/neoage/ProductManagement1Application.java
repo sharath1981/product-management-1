8df06bfa46c0572dbf6ba8bc833e350d146c96fc
SHARATH KUMAR B <sharath1981@gmail.com>
2025-07-19 23:10:13 -0500

    Initial Commit

---
+++ src/main/java/com/neoage/ProductManagement1Application.java
@@ -0,0 +1,13 @@
+package com.neoage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagement1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoage/ProductManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
